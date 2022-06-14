120a3ff69265e76a2a334df85045a6790bbd4239
Luiz Mainart <luizdmmainart@gmail.com>
2022-06-14 14:39:44 -0300

    feat: Navbar styling
    Added styling to Navbar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { ReactComponent as Logo } from 'assets/crown.svg'
+import { Link, Outlet } from 'react-router-dom'
+import './navigation.styles.scss'
 
 const Navigation = () => {
   return (
     <div className="App">
-      <nav>
-        <h1>I am the navigation</h1>
+      <nav className="navigation">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
       </nav>
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </div>
   )
 }
